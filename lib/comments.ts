018ea85d8ee7906498e6eb4456864258eac424d9
@nxora <omotoyedave4@gmail.com>
2025-12-05 11:07:11 +0100

    Created created user model for next-auth flow

--- lib/comments.ts
+++ lib/comments.ts
@@ -2,27 +2,27 @@ import mongoose from "mongoose";
 import { connectDB } from "./db";
 import Comment from "@/models/commentmodel";
 
-export interface CommentInput{
-     postId: {type: String},
-     authorId: {type: String, ref: "User"},
-    content: string;
- }
- export async function getAllPostComments(id: string) {
+export interface CommentInput {
+  postId: string;
+  authorId: string;
+  content: string;
+}
+export async function getAllPostComments(id: string) {
     await connectDB()
-    return Comment.find({_id: id})
+    return Comment.find({postId: id})
 }
 
 export async function createComment(data: CommentInput){
     await connectDB()
-    return Comment.create({data})
+    return Comment.create(data)
 }
 
 export async function deleteComment(id: string){
     await connectDB()
-    return Comment.findByIdAndDelete({_id: id})
+    return Comment.findByIdAndDelete(id)
 }
 
 export async function UpdateComment(id: string){
     await connectDB()
-    return Comment.findByIdAndUpdate({_id: id})
+    return Comment.findByIdAndUpdate(id, DataTransfer,{new: true})
 }
--- models/usermodel.ts
+++ models/usermodel.ts
@@ -1,11 +1,23 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-export default mongoose.models.User || mongoose.model("User", userSchema);
+// hash before saving
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+
+
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+export default User;
